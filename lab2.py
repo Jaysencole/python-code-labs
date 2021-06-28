8f18141c2947d58559694e4a510f5e77a87b81ea
Jaysencole <86015639+Jaysencole@users.noreply.github.com>
2021-06-28 12:28:58 -0700

    Adding lab 1 and 2

---
+++ lab2.py
@@ -0,0 +1,20 @@
+name = "lebron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+
+
+My_name = "Jaysen"
+work = False
+height = 6.0
+weight = 260
+school = "mcclymonds"
+
+
+
+print(type(My_name))
+print(type(work))
+print(type(height))
+print(type(weight))
+print(type(school))
